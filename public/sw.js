35b5a8dc82714bc03e4d5ecf7d1a9cac735e4282
Giorgio <gaionifelappi@gmail.com>
2026-09-10 00:41:35 +0200

    v1.2.2: - added icons - update manifest - cache support

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,9 @@ const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/favicon.svg'
+  '/favicon.svg',
+  '/pwa-192x192.png',
+  '/pwa-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
